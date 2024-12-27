5388295959fb6a2e4acad8d77a0c14854679d89d
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-12-26 23:29:50 -0300

    Refactor- ajuste de estilos responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,11 +8,12 @@ export default {
 				manrope: ['"Manrope"', 'sans-serif'],
 			},
 			screens: {
-				sm: '400px',
-				md: '768px',
-				lg: '1024px',
-				xl: '1400px',
-				xxl: '1920px',
+				'sm': '400px',      // Pantallas pequeñas
+				'md': '768px',      // Pantallas medianas
+				'lg': '1024px',     // Pantallas grandes (tabletas, etc.)
+				'xl': '1280px',     // Laptops medianas
+				'xlg': '1280px',  // Pantallas entre 1280px y 1920px (breakpoint intermedio)
+				'xxl': '1920px',    // Pantallas grandes (pantallas más grandes que 1920px)
 			},
 			colors: {
 				primary: '#929f36',
