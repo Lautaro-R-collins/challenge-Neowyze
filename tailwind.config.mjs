8b56bc806e91d6a2307e3bc40bf17b5773b3acb4
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-12-27 10:50:14 -0300

    Refactor-code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
 				manrope: ['"Manrope"', 'sans-serif'],
 			},
 			screens: {
-				'sm': '400px',      
+				'sm': '550px',      
 				'md': '768px',      
 				'lg': '1024px',     
 				'xl': '1280px',   
